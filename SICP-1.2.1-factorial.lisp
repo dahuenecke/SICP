509269fee2a283dbf54191757258be5b0f415847
dahuenecke <dhuenecke@outlook.com>
2019-02-09 15:59:13 -0600

    added iterative process factorial

--- SICP-1.2.1-factorial.lisp
+++ SICP-1.2.1-factorial.lisp
@@ -1,4 +1,13 @@
+;recursive process
 (define (factorial x)
     (if (= x 1)
         1
         (* n (factorial (- n 1)))))
+
+;iterative process
+(define (factorial n)
+    (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+    (if (> counter max-counter)
+        product
+        (fact-iter (* counter product) (+ counter 1) max-count)))
